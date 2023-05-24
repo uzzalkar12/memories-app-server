5548196ef86358f27849d3ef0594f0630949fa5f
Uzzal Kar <uzzalkar12@gmail.com>
2023-05-24 23:24:17 +0600

    router index modify

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 // import postRoutes from './routes/posts.js'
-import rootRouter from "./routes/index.js";
+// import rootRouter from "./routes/index.js";
+import posts from "./routes/posts.js";
+import categories from "./routes/categories.js";
 
 const app = express();
 dotenv.config();
@@ -13,7 +15,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('', rootRouter)
+app.use('/posts', posts);
+app.use('/categories', categories);
+
+// app.use('', rootRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello to memories API');
