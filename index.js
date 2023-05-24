4bfb12263d8dba37ae809785e8f3638c7900b24c
Uzzal Kar <uzzalkar12@gmail.com>
2023-05-24 23:12:35 +0600

    router index modify

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/', rootRouter)
+app.use('', rootRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello to memories API');
