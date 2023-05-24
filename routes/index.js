91b63a0cde27108620f25dd2c9a1debd68cde6e4
Uzzal Kar <uzzalkar12@gmail.com>
2023-05-24 22:48:58 +0600

    router index modify

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,14 @@ import express from "express";
 import posts from './posts.js';
 import categories from './categories.js';
 
-let rootRouter = express.Router();
+// let rootRouter = express.Router();
 
-rootRouter.use('/posts', posts);
-rootRouter.use('/categories', categories);
+// rootRouter.use('/posts', );
+// rootRouter.use('/categories', categories);
 
-export default rootRouter;
+// export default rootRouter;
+
+module.exports = (app) => {
+    app.use("/posts", posts);
+    app.use("/categories", categories);
+};
